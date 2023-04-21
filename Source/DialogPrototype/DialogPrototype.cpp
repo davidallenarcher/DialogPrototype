7fa93bc5ace9361ace669b865cf15c8afe57cc0d
David Allen Archer <David.Allen.Archer@gmail.com>
2023-04-21 16:59:30 -0400

    first commit

---
+++ Source/DialogPrototype/DialogPrototype.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DialogPrototype.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DialogPrototype, "DialogPrototype" );
